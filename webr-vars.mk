e74c183ce65e8cfccb6f3235f1393fd848782ab9
George Stagg <georgestagg@gmail.com>
2023-02-14 09:27:30 +0000

    Explicitly set R_HOME for wasm build (#10)

--- webr-vars.mk
+++ webr-vars.mk
@@ -7,6 +7,7 @@
 
 R_VERSION = $(shell cat $(WEBR_ROOT)/R/R-VERSION)
 R_SOURCE = $(WEBR_ROOT)/R/build/R-$(R_VERSION)
+R_HOME = $(WEBR_ROOT)/wasm/R-$(R_VERSION)/lib/R
 
 WEBR_INCLUDES = -I$(R_SOURCE)/build/include -I$(R_SOURCE)/src/include
 WEBR_LDFLAGS = -L$(WEBR_ROOT)/wasm/lib -L$(WEBR_ROOT)/wasm/R-$(R_VERSION)/lib/R/lib
