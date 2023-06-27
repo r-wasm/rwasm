631cf77b18eb71147599a983de4e6b8e82f09025
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-06-27 18:36:24 +0200

    fix: needs to use {zip} package to perserve permissions

--- repo-update.R
+++ repo-update.R
@@ -76,7 +76,7 @@ make_remote_tarball <- function(pkg, url, target) {
     # Get root folder name, necessary as it won't unzip as `pkg`
     folder_name <- unzip(source_tarball, list = TRUE)$Name[[1]]
     
-    unzip(source_tarball, exdir = file.path(tmp_dir))
+    zip::unzip(source_tarball, exdir = file.path(tmp_dir))
     
     # rename folder_name to `pkg`
     file.rename(file.path(tmp_dir, folder_name), file.path(tmp_dir, pkg))
