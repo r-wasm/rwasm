bd0b9ac185b0ddef97572c70411495e0b4fc49a8
George Stagg <george.stagg@posit.co>
2023-10-03 09:21:38 +0100

    Add function to make Emscripten VFS image

--- R/lib.R
+++ R/lib.R
@@ -7,3 +7,35 @@ make_library <- function(repo_dir = "./repo", lib_dir = './lib') {
   lapply(pkgs, function(pkg) { untar(pkg[[1]], exdir = lib_dir) })
   invisible(0)
 }
+
+make_vfs_image <- function(out_dir = './vfs', lib_dir = './lib', ...) {
+  make_library(...)
+  fs::dir_create(out_dir)
+
+  file_packager <- fs::path(
+    getOption("rwasm.emsdk_root"),
+    "upstream",
+    "emscripten",
+    "tools",
+    "file_packager"
+  )
+
+  lib_abs <- fs::path_abs(lib_dir)
+  res <- withr::with_dir(
+    out_dir,
+    system2(file_packager,
+      args = c(
+        "library.data", "--preload", paste0("'", lib_abs, "@/'"),
+        "--separate-metadata", "--js-output='library.js'"
+      ),
+      stdout = TRUE,
+      stderr = TRUE
+    )
+  )
+
+  status <- attr(res, "status")
+  if (!is.null(status) && status != 0) {
+    stop("An error occurred running `file_packager`:\n",
+         paste(res, collapse = '\n'))
+  }
+}
